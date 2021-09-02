733eeac47b661015cc955487511095b1cd7ec240
Elias Lima <eliasv.lima@yahoo.com.br>
2021-09-01 22:56:15 -0300

    Container Docker Php - MySQL - Apache

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<h1>Hello there, fellow Developer!</h1>
+
+<?php
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+
+?>
